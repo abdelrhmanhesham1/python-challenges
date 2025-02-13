44d75d7620c2475842fa63c3a68a79effbd685fd
abdelrhmanhesham1 <abdelrhman.hesham108@gmail.com>
2025-02-14 01:16:32 +0200

    Create problem2- arithmetic operation.py

---
+++ problem2- arithmetic operation.py
@@ -0,0 +1,24 @@
+The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+
+==># Read two integers from input
+a = int(input())
+b = int(input())
+
+# Print the sum with the expression
+print(f"{a} + {b} = {a + b}")
+
+# Print the difference with the expression
+print(f"{a} - {b} = {a - b}")
+
+# Print the product with the expression
+print(f"{a} * {b} = {a * b}")
+
+==>5, 3
+5 + 3 = 8
+5 - 3 = 2
+5 * 3 = 15
+
