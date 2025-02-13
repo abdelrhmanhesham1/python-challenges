d0d9591ed1320db9bf4cfb7e5ad7ac86296d440b
abdelrhmanhesham1 <abdelrhman.hesham108@gmail.com>
2025-02-14 01:20:25 +0200

    Create problem3- division.py

---
+++ problem3- division.py
@@ -0,0 +1,11 @@
+Add logic to print two lines. The first line should contain the result of integer division. 
+The second line should contain the result of float division.
+
+==>  a = int(input())
+    b = int(input())
+
+print(a // b)  # Integer division
+print(a / b)   # Float division
+==>5, 2
+integer division = 2
+float division = 2.5
